611fe242d64d4e3083ff0cf9f71003bef0023aa2
dmathieu <kazhar@gmail.com>
2009-02-19 13:34:41 +0100

    Show emailing form and send emails
    Signed-off-by: dmathieu <kazhar@gmail.com>

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -1,7 +1,61 @@
 <?php
-class IndexController extends Zend_Controller_Action  {
+class IndexController extends AppGlobalController  {
 	
 	public function indexAction() {
 		
+	}
+	
+	public function emailsAction() {
+		$nb = $this->_getParam('nb');
+		$this->setForm($nb);
+	}
+	
+	public function sendAction() {
+		$nb = $this->_getParam('nb');
+		$form = new Form_Email($nb);
+		$request = $this->getRequest();
+		
+		if ($this->getRequest()->isPost()) {
+			if ($form->isValid($request->getPost())) {
+				$datas = $request->getPost();
+				
+				$emails = array();
+				foreach($datas as $i => $uplet) {
+					if (preg_match('`email([0-9]+)`', $i)) {
+						$emails[] = $uplet;
+					}
+				}
+				$nicknames = Nickname::getInstance()->get_rand($datas['category'], count($emails));
+				
+				if (count($emails) > count($nicknames)) {
+					$this->setForm($form);
+					return $this->render('emails');
+				}
+				
+				foreach($emails as $i => $email) {
+					//We send the email with the nickname
+					$mail = new Zend_Mail();
+					$mail->setBodyText('Votre pseudonyme pour Quake III est : '.$nicknames[$i]['name'])
+					->setFrom('noreply@quakerox.o2sources', 'Quake')
+					->addTo($email)
+					->setSubject('Votre pseudonyme pour Quake III')
+					->send();
+				}
+				$this->flash()->addMessage('Les emails ont correctement été envoyés.');
+				$this->_helper->redirector('index', 'index');
+			} else {
+				$this->setForm($form);
+				return $this->render('emails');
+			}
+		} else
+			return $this->_helper->redirector('emails', 'index');
+	}
+	
+	protected function setForm($form) {
+		if (ereg('([0-9]+)', $form))
+			$form = new Form_Email($form);
+			
+		$form->setAction('/index/send?nb='.$form->number());
+		$this->view->form = $form;
 	}
 }
